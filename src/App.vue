01917c44b75d05ce6ca8912e4ef75303fe60bc7e
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 23:07:13 +0200

    170 creating a local filter

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,25 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>{{ text | toUppercase }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      text: "Hello World!"
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
     }
+  }
+};
 </script>
 
 <style>
